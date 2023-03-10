d4dd848ee75ae5012a4f1766b70f12ee312c27f5
Kiyotaka-237 <draculemihawk232@gmail.com>
2023-03-10 12:01:39 +0100

    table

---
+++ ASCII/table.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+    char *table[] = {"table"};
+    int i, j = 0;
+
+    if ((strcmp(argv[1], *table) == 0)) {
+        for (i = 0; i < 127; i++) {
+						if (i <= 31)
+							printf(" |%3d - np|", i);
+						else
+            	printf(" |%3d -  %c|", i, i);
+            if (i % 6 == 0)
+                printf("\n");
+        }
+        printf("\n");
+        return 0;
+    }
+    else printf("%s: argument not found", argv[1]);
+    return -1;
+}
