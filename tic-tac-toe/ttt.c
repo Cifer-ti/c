5db899445d70bb3090df4007f65db7edd48d47a6
Cifer-ti <draculemihawk232@gmail.com>
2023-07-26 23:57:31 +0100

    added board funciton

--- tic-tac-toe/ttt.c
+++ tic-tac-toe/ttt.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include <stdlib.h>
+#include <ctype.h>
 
+bool bord_status[9] = {true};
 char square[] = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
 
 int checkwin(void);
@@ -14,38 +17,63 @@ int main(void)
 	char mark;
 
 	do {
+		board();
 		player = (player % 2) ? 1 : 2;
+		printf("%d\n", player);
 
-		printf("player %d enter a square number number\n", player);
+		printf("player %d enter a square f number: ", player);
 		scanf("%d", &choice);
 
-		mark = (player == 1) ? 'X' : 'O';
+		if (choice >= 1 || choice <= 9) {
 
-		ash = checkchioce(choice);
+			mark = (player == 1) ? 'X' : 'O';
 
-		if ( ash > 0)
-			square[ash] = mark;
+			ash = checkchoice(choice);
+
+			if ( ash > 0) {
+				printf("fine play\n");
+				square[choice - 1] = mark;
+				player++;
+			}
+			else
+				printf("Illegal move\n");
+		}
 		else
-			printf("Illegal move");
+			printf("choice has to be a number bewtween 1-9\n try again\n");
 			
-	}
+	} while (1);
 }
 
 int checkchoice(int Choice)
 {
-
-	int i;
-
-	for (i = 0; i < 9; i++) {
-		if (square[i] == Choice)
-			return (i);
-	}
-
-	return -1;
+	if (bord_status[Choice])
+		return -1;
+	bord_status[Choice] = true;
+	return 1;
 }
 
+/*
 int checkwin(void)
 {
-	if (square[0] == square[1] && square[1] == square[2])
+	if (bord_status[0] == bord_status[1] && bord_status[1] == bord_status[2])
 		return 1;
-	if (square[
+	if (bord_status[*/
+
+
+void board(void)
+{
+	int i;
+	
+	for (i = 0; i <= 8; i++) {
+		printf("| %c |", square[i]);
+		if (( (i + 1) % 3 ) == 0)
+			printf("\n");
+	}
+}
+
+
+
+
+
+
+
