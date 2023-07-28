62d79e615c78ce3d23a1b7487a19e741a08ebad3
Cifer-ti <draculemihawk232@gmail.com>
2023-07-28 22:30:51 +0100

    little touches

--- tic-tac-toe/ttt.c
+++ tic-tac-toe/ttt.c
@@ -3,44 +3,52 @@
 #include <stdlib.h>
 #include <ctype.h>
 
+/*defintion of extenal variables*/
 int bord_status[9] = {0};
-bool fin[9] = {0};
 char square[] = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
-int comp = 0;
 
+/*function prototypes*/
 int checkwin(void);
 int checkchoice(int Choice);
 void board(void);
+void error_prompt(void);
 
 int main(void)
 {
-	int player = 1, i, choice, ash;
-	int gol = 0;
-
+	int player = 1, i, choice, check;
+	int has_won = 0;
 	char mark;
 
 	do {
 		board();
 		player = (player % 2) ? 1 : 2;
-		printf("%d\n", player);
 
-		printf("player %d enter a square f number: ", player);
-		scanf("%d", &choice);
+		printf("player %d enter the number of a square: ", player);
+		if (scanf("%d", &choice) != 1) {
+			getchar();
+			choice = -1;
+		}
 
 		
-		if (choice >= 1 || choice <= 9) {
+		if (choice >= 1 && choice <= 9) {
 
 			mark = (player == 1) ? 'X' : 'O';
 
-			ash = checkchoice(choice);
+			check = checkchoice(choice);
 
-			if ( ash > 0) {
+			if ( check > 0) {
 				printf("\nfine play\n");
 				square[choice - 1] = mark;
-				gol = checkwin();
+				has_won = checkwin();
 				
-				if (gol > 0) {
-					printf("Player %d win", player);
+				if (has_won == 0) {
+					board();
+					printf("Draw game: No more empty square left\n");
+					return 0;
+				}
+				if (has_won > 0) {
+					board();
+					printf("Player %d win\n", player);
 					return 0;
 				}
 				player++;
@@ -49,20 +57,7 @@ int main(void)
 				printf("Illegal move\n");
 		}
 		else
-			printf("choice has to be a number bewtween 1-9\n try again\n");
-		
-		/*
-		printf("bo: ");
-		for (int i = 0; i < 9; i++)
-			printf("%d", bord_status[i]);
-
-		printf("\n"); 
-		printf("q: ");
-		for (int i = 0; i < 9; i++)
-			printf("%c", square[i]);
-		printf("\n");
-		*/
-			
+			error_prompt();	
 	} while (1);
 }
 
@@ -75,23 +70,23 @@ int checkchoice(int Choice)
 	return 1;
 }
 
-/*
-int checkwin(void)
-{
-	if (bord_status[0] == bord_status[1] && bord_status[1] == bord_status[2])
-		return 1;
-	if (bord_status[*/
 
+void error_prompt(void)
+{
+	printf("\nchoice has to be a number between 1-9\ntry again\n\n");
+}
 
 void board(void)
 {
 	int i;
 	
+	printf("\n");
 	for (i = 0; i <= 8; i++) {
 		printf("| %c |", square[i]);
 		if (( (i + 1) % 3 ) == 0)
 			printf("\n");
 	}
+	printf("\n");
 }
 
 int checkwin()
