96c026a015ffaa5350674609a53a62698835a620
Cifer-ti <draculemihawk232@gmail.com>
2023-08-01 22:54:14 +0100

    started voting system program

---
+++ voting_system/vote.c
@@ -0,0 +1,4 @@
+/* This file will contain the main function */
+I don't really know how to start this 
+like I don't know how it works... well I think I should try to search on how this thing first works.
+
