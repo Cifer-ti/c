54a64bbcf81225e7e9e7b0be46c0982fa65e9801
Cifer-ti <draculemihawk232@gmail.com>
2023-08-06 00:01:54 +0100

    started implementing functions

---
+++ voting_system/candidate/candidate.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "candidate.h"
+
+struct candidate {
+	char *candidate_name;
+	char candidate_id[7];
+	int num_voates;
+	bool banned;
+};candidate
+
+struct election_type {
+	char election_id[7];
+	struct candidate *top;
+};
+
+/*functions*/
+
+PRIVATE void terminate(const char *message)
+{
+	printf("%s\n", message);
+	exit(EXIT_FAILURE);
+}
+candidate
+PUBLIC create_election(void)
+{
+	election el = malloc(sizeof(struct election_type));
+
+	if (el == NULL)
+		terminate("\nerror in create: Election could not be started\n");
+
+	el->top = NULL;
+	return el;
+}
--- voting_system/candidate/candidate.h
+++ voting_system/candidate/candidate.h
@@ -1,12 +1,16 @@
 #ifndef CANDIDATE_H
 #define CANDIDATE_H
 
-typedef sruct election_type *election;
+/*tells if a function is to be used out of file or not*/
+#define PUBLIC
+#define PRIVATE static
 
-election create_election(void);
-void make_candidate(electon elect);
-void delete_candidate(election elect);
-void ban_candidate(election elect);
-void vote(election elect);
+typedef struct election_type *election;
+
+PUBLIC create_election(void);
+PUBLIC void make_candidate(electon elect);
+PUBLIC void delete_candidate(election elect);
+PUBLIC void ban_candidate(election elect);
+PUBLIC void vote(election elect);
 
 #endif
