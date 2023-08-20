22853f318b171f7a3fdd162d571e966e1da2b22c
Cifer-ti <draculemihawk232@gmail.com>
2023-08-20 14:54:52 +0100

    commneting functions

--- voting_system/adminisatrative/admin.h
+++ voting_system/adminisatrative/admin.h
@@ -5,8 +5,16 @@
 #define PUBLIC
 #define PRIVATE static
 
+/*checkadminlogin: checks if the username and password provided
+                    are those of the admin for login*/
 PUBLIC int checkadminlogin(void);
+
+/*changeadminname: changes the admin name from the current one to 
+                    one provide by the user*/
 PUBLIC void changeadminname(void);
+
+/*changeadminpasswd: changes the admin password from the
+                        current one to one provided by the user*/
 PUBLIC void changeadminpasswd(void);
 
 #endif
