037e19f9f55b548261fc5f923391e1c8d981771e
Cifer-ti <draculemihawk232@gmail.com>
2023-08-28 21:38:05 +0100

    comment: added some comment to the ASCII program

--- ASCII/ASCII_n/char.c
+++ ASCII/ASCII_n/char.c
@@ -1,3 +1,5 @@
+/* Prints a character from the ASCII table  
+*  when given it's corresponding number*/
 #include <stdio.h>
 #include "char.h"
 
@@ -10,5 +12,5 @@ void ch(void) {
     if (value >= 0 && value <= 31)
         printf("characters with value of 0 - 32 are unprintable");
     else
-    printf("character of %d - %c\n", value, value);
+        printf("character of %d - %c\n", value, value);
 }
