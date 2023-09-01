5b957ac7fd83b93d825bb7a60b00f575837bc074
Cifer-ti <draculemihawk232@gmail.com>
2023-09-01 23:50:38 +0100

    refactoring code

--- ASCII/ASCII_cmd/table.c
+++ ASCII/ASCII_cmd/table.c
@@ -3,9 +3,9 @@
 #include <stdio.h>
 #include <string.h>
 
+char *table[] = {"--table", "--character", "--value", "--help"};
 int main(int argc, char *argv[]) {
-    char *table[] = {"--table", "--help"};
-    int i, j = 0;
+    int i, j, a;
 
     if(argc != 2) {
         printf("Usage: %s arguement.\n", *argv);
@@ -13,26 +13,38 @@ int main(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     }
 
-    if((strcmp(argv[1], *table) == 0)) {
-        for (i = 0; i <= 127; i++) {
-            if (i <= 31)
-                printf(" |%3d - np|", i);
-            else
-                printf(" |%3d -  %c|", i, i);
-            if (i % 6 == 0)
-            printf("\n");
+    for(i = 0, j = 1; table[i] != NULL; i++) {
+        if(strcmp(argv[j], table[i]) == 0) {
+            a = i;
+            i = j = 0;
+            break;
         }
-        printf("\n");
-        return 0;
     }
 
-    if((strcmp(argv[1], table[1]) == 0)) {
-        printf("\nhelp page\n"
+    switch(a) {
+        case 0:
+            for (i = 0; i <= 127; i++) {
+                if (i <= 31)
+                    printf(" |%3d - np|", i);
+                else
+                    printf(" |%3d -  %c|", i, i);
+                if (i % 6 == 0)
+                printf("\n");
+            }
+            printf("\n");
+            break;
+        
+        case 1:
+            printf("\nhelp page\n"
                 "Valid arguements:\n"
                 "--table\t print ASCII table\n"
                 "--help\t print this help\n");
-        return 0;
+            break;
+        
+        default:
+            printf("Argument %s not found\n %s --help for help\n", argv[1], *argv);
+            return -1;
     }
-    else printf("Argument %s not found\n %s --help for help\n", argv[1], *argv);
-    return -1;
+
+    return 0;
 }
