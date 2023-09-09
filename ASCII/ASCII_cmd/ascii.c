f2f7652ba1968c25ebf7d7e41034e30f3c177213
Cifer-ti <draculemihawk232@gmail.com>
2023-09-09 23:59:38 +0100

    some refactoring

--- ASCII/ASCII_cmd/ascii.c
+++ ASCII/ASCII_cmd/ascii.c
@@ -1,27 +1,34 @@
 //print the ASCII table using a command line program
+#include <ctype.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
-char *table[] = {"--table", "--character", "--value", "--help"};
+char *options[] = {"--table", "--val", "--char", "--help"};
+
+int char_to_value(char ch);
+int value_to_char(int val);
+void help(char *prog_name);
+
 int main(int argc, char *argv[]) {
-    int i, j, a;
+    int i, arg_index = -1;
 
-    if(argc != 2) {
-        printf("Usage: %s arguement.\n", *argv);
-        printf("%s --help for details\n", *argv);
-        exit(EXIT_FAILURE);
-    }
+    if(argc < 2)
+        help(argv[0]);
 
-    for(i = 0, j = 1; table[i] != NULL; i++) {
-        if(strcmp(argv[j], table[i]) == 0) {
-            a = i;
-            i = j = 0;
+    for(i = 0; options[i] != NULL; i++) {
+        if(strcmp(argv[1], options[i]) == 0) {
+            arg_index = i;
             break;
         }
     }
 
-    switch(a) {
+    if(options[i] == NULL) {
+        printf("Error: check arguements.\n");
+        help(argv[0]);
+    }
+
+    switch(arg_index) {
         case 0:
             for (i = 0; i <= 127; i++) {
                 if (i <= 31)
@@ -35,6 +42,10 @@ int main(int argc, char *argv[]) {
             break;
         
         case 1:
+            value_to_char(atoi(argv[2]));
+            break;
+        
+        case 4:
             printf("\nhelp page\n"
                 "Valid arguements:\n"
                 "--table\t print ASCII table\n"
@@ -47,4 +58,35 @@ int main(int argc, char *argv[]) {
     }
 
     return 0;
+}
+
+int char_to_value(char ch) 
+{
+ 
+
+    printf("Enter charater to get ASCII value: ");
+    scanf(" %c", &ch);
+
+    printf("character of %c - %d\n", ch, ch);
+}
+
+int value_to_char(int val) 
+{
+
+    if(!isdigit(val))
+        return -1;
+
+    if (val >= 0 && val <= 31)
+        printf("characters with value of 0 - 32 are unprintable");
+    else
+        printf("character of %d - %c\n", val, val);
+    
+    return 0;
+}
+
+void help(char *prog_name)
+{
+        printf("Usage: %s arguement.\n", *prog_name);
+        printf("%s --help for details\n", *prog_name);
+        exit(EXIT_FAILURE);
 }
