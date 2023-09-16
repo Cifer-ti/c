fd267c39e45084bb617775bcb728980215b0db5e
Cifer-ti <draculemihawk232@gmail.com>
2023-09-16 21:40:59 +0100

    commented

--- ASCII/ASCII_cmd/ascii.c
+++ ASCII/ASCII_cmd/ascii.c
@@ -6,19 +6,57 @@
 
 char *options[] = {"--table", "--val", "--char", "--help"};
 
-int char_to_value(char *str_ch);
-int value_to_char(char *val);
-void error(char *prog_name);
+/**
+ * char_to_value: Takes the string passed to it, and 
+ *                extracts the first charater from it,
+ *                and prints it's equivalent ASCII value.
+*/
+void char_to_value(char *str_ch);
+
+/**
+ * value_to_char: Takes the string passed to it, 
+ *                checks if the string is fully made up of integers
+ *                if it is not it returns -1 showing a conversion fail
+ *                if it is, it then converts it to the corresponding integer,
+ *                and checks if it is positive and less than 127 before continuing to
+ *                the appropriate printing of the number and it's ASCII character.
+ * 
+ * *str_val- A pointer the value to be converted.
+ * 
+ * Return- -1 if an error occured, and 0 for succesful execution.
+*/
+int value_to_char(char *str_val);
+
+/**
+ * error: prints an error message, a small usage
+ *         exampls and a hint to
+ *         using the help option for usage details.
+ * 
+ * *prog_name- A pointer to the name of the running program.
+*/
+void error(char *progs_name);
+
+/**
+ * help: Prints a help page showing how to use the program
+ *       with the different valid options.
+ * 
+ * *prog_name- A pointer to the name of the running program.
+*/
 void help(char *prog_name);
+
+/**
+ * table: Prints a complete view of the ASCII table.
+*/
 void table(void);
 
 int main(int argc, char *argv[]) 
 {
-    int i, option_index = -1;
+    int i, option_index;
 
     if(argc < 2)
         error(*argv);
 
+    /* check arguements match with valid options */
     for(i = 0; options[i] != NULL; i++) {
         if(strcmp(argv[1], options[i]) == 0) {
             option_index = i;
@@ -26,11 +64,13 @@ int main(int argc, char *argv[])
         }
     }
 
+    /* no match found */
     if(options[i] == NULL) {
         printf("Error: %s invalid arguement.\n", argv[1]);
         error(*argv);
     }
 
+    /* if arguement correspoind to --val or --char, then a third arguement needs to be there*/
     if((option_index == 1 || option_index == 2) && argc != 3) {
         printf("Error: no arguement to be converted\n");
         error(*argv);
@@ -77,7 +117,7 @@ void table(void)
 
 }
 
-int char_to_value(char *str_ch) 
+void char_to_value(char *str_ch) 
 {
     char char_ch;
 
@@ -85,17 +125,17 @@ int char_to_value(char *str_ch)
     printf("character of %c - %d\n", char_ch, char_ch);
 }
 
-int value_to_char(char *val) 
+int value_to_char(char *str_val) 
 {
     int int_val;
     char *endptr;
 
+    /* check if arguement string has an integer*/
     strtol(val, &endptr, 10);
-
     if(*endptr != '\0')
-        return -1;
+        return -1;      /* arguement string does is not or not totaly integers */
     
-    int_val = atoi(val);
+    int_val = atoi(val);    /* exttract value from string and convert to int */
 
     if (int_val >= 0 && int_val <= 31)
         printf("characters with value of 0 - 32 are unprintable\n");
